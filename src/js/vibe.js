8c12fc899cebfc412be4454f62cb915b87684578
Kevin Clelland <kevinclelland@gmail.com>
2020-11-29 16:48:53 +0200

    Fixing up

--- src/js/vibe.js
+++ src/js/vibe.js
@@ -1,9 +1,13 @@
  // Bank some good vibe options
  const vibes = [
-  "...Cause when I see you, it's like I'm staring down the sun...",
-  "...It's too late to repent, don't bother with the rent...",
-  "...Someday I'll wear pyjamas in the daytime...",
-  "...I want to have the same last dream again, The one where I wake up and I'm alive..."
+  "'...Cause when I see you, it's like I'm staring down the sun...'",
+  "'...It's too late to repent, don't bother with the rent...'",
+  "'...Someday I'll wear pyjamas in the daytime...'",
+  "'...And I'm here, to remind you Of the mess you left when you went away...'",
+  "'...I just want to be who I want to be Guess that's hard for others to see...'",
+  "'...I can't take my eyes off you...'",
+  "'...You're calling too late too late to be gracious you do not warrant long goodbyes...'",
+  "'...I want to have the same last dream again, The one where I wake up and I'm alive...'"
 ];
 
  // choose a random good vibe
