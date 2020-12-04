f1a7647dc7fceb6eb8444347492f0580db6a330f
Kevin Clelland <kevinclelland@gmail.com>
2020-12-04 21:39:26 +0200

    Add inputs page

--- src/_data/lastFM.js
+++ src/_data/lastFM.js
@@ -3,7 +3,7 @@
 
  module.exports = async function() {
   try {
-    const response = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${process.env.LASTFM_API_KEY}&artist=crash test dummies&album=god shuffled his feet&format=json`);
+    const response = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${process.env.LASTFM_API_KEY}&artist=blue october&album=foiled&format=json`);
     return response.data;
   } catch (error) {
     console.error(error);
