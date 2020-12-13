26eb79478cc7a65553f364ec2720bd184dd474dc
Kevin Clelland <kevinclelland@gmail.com>
2020-12-13 20:11:27 +0200

    Removing some copy, adding a profile

--- src/js/vibe.js
+++ src/js/vibe.js
@@ -3,11 +3,11 @@
   "'...Cause when I see you, it's like I'm staring down the sun...'",
   "'...It's too late to repent, don't bother with the rent...'",
   "'...Someday I'll wear pyjamas in the daytime...'",
-  "'...And I'm here, to remind you Of the mess you left when you went away...'",
-  "'...I just want to be who I want to be Guess that's hard for others to see...'",
+  "'...And I'm here, to remind you, of the mess you left when you went away...'",
+  "'...I just want to be who I want to be. Guess that's hard for others to see...'",
   "'...I can't take my eyes off you...'",
-  "'...You're calling too late too late to be gracious you do not warrant long goodbyes...'",
-  "'...I want to have the same last dream again, The one where I wake up and I'm alive...'"
+  "'...You're calling too late. Too late to be gracious. You do not warrant long goodbyes...'",
+  "'...I want to have the same last dream again. The one where I wake up and I'm alive...'"
 ];
 
  // choose a random good vibe
