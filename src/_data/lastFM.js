4edc2a2965a82f7e67641fcf55e2726a2075e4dd
Kevin Clelland <kevinclelland@gmail.com>
2020-12-19 14:01:04 +0200

    Added Profile

--- src/_data/lastFM.js
+++ src/_data/lastFM.js
@@ -1,9 +1,11 @@
  const axios = require('axios')
  require('dotenv').config()
+ var artistName = "coldplay";
+ var albumName = "parachutes";
 
  module.exports = async function() {
   try {
-    const response = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${process.env.LASTFM_API_KEY}&artist=blue october&album=foiled&format=json`);
+    const response = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${process.env.LASTFM_API_KEY}&artist=${artistName}&album=${albumName}&format=json`);
     return response.data;
   } catch (error) {
     console.error(error);
