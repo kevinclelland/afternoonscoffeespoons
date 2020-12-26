ef825ec134709e663f3e2d31784323309b481985
Kevin Clelland <kevinclelland@gmail.com>
2020-12-26 15:27:04 +0200

    Added profile, amended SA Rocks

--- src/_data/lastFM.js
+++ src/_data/lastFM.js
@@ -1,19 +1,17 @@
  const axios = require('axios')
  require('dotenv').config()
- var artistName = "coldplay";
- var albumName = "parachutes";
+ var artistName1 = "The Offspring";
+ var albumName1 = "Smash";
+ var artistName2 = "Shotgun Tori";
+ var albumName2 = "Be Brave";
 
  module.exports = async function() {
   try {
-    const response = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${process.env.LASTFM_API_KEY}&artist=${artistName}&album=${albumName}&format=json`);
+    const response = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${process.env.LASTFM_API_KEY}&artist=${artistName1}&album=${albumName1}&format=json`);
     return response.data;
   } catch (error) {
     console.error(error);
   }
 
 };
-
-
-
-
 
