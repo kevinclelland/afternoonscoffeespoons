117e5fa0c0b18fa5127629c869f715332992c90a
Kevin Clelland <kevinclelland@gmail.com>
2021-01-10 20:50:46 +0200

    Added cassete player

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function(config) {
   config.addPassthroughCopy("src/js");
   config.addPassthroughCopy("src/assets/img");
   config.addPassthroughCopy("src/assets/css");
+  config.addPassthroughCopy("src/songs");
+  config.addPassthroughCopy("src/sounds");
   return  {
     dir: {
       input: "src",
